f9c614a4cd105c3525605244f7da1f949c712e8c
Banik1103 <jeremi.kilimnik@gmail.com>
2021-04-05 16:40:51 +0200

    pause and resume are combined into one button

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 
+String _title = "Pause";
+
 void main() {
   runApp(MyApp());
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
@@ -10,7 +12,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: MyHomePage());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
 
@@ -26,111 +28,87 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Pomodoro Clock'),
-        centerTitle: true,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.settings),
-            onPressed: () {},
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, this.title}) : super(key: key);
-
-  final String? title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  CountDownController _controller = CountDownController();
-  int _duration = 10;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title!),
-      ),
+      appBar: AppBar(title: Text('Pomodoro Clock'), centerTitle: true),
       body: Center(
+        child: Padding(
+          padding:
+              EdgeInsets.only(bottom: MediaQuery.of(context).size.height / 7),
           child: CircularCountDownTimer(
-        // Countdown duration in Seconds.
-        duration: _duration,
+            // Countdown duration in Seconds.
+            duration: _duration,
 
-        // Countdown initial elapsed Duration in Seconds.
-        initialDuration: 0,
+            // Countdown initial elapsed Duration in Seconds.
+            initialDuration: 0,
 
-        // Controls (i.e Start, Pause, Resume, Restart) the Countdown Timer.
-        controller: _controller,
+            // Controls (i.e Start, Pause, Resume, Restart) the Countdown Timer.
+            controller: _controller,
 
-        // Width of the Countdown Widget.
-        width: MediaQuery.of(context).size.width / 2,
+            // Width of the Countdown Widget.
+            width: MediaQuery.of(context).size.width / 2,
 
-        // Height of the Countdown Widget.
-        height: MediaQuery.of(context).size.height / 2,
+            // Height of the Countdown Widget.
+            height: MediaQuery.of(context).size.height / 2,
 
-        // Ring Color for Countdown Widget.
-        ringColor: Colors.grey[300]!,
+            // Ring Color for Countdown Widget.
+            ringColor: Colors.grey[300]!,
 
-        // Ring Gradient for Countdown Widget.
-        ringGradient: null,
+            // Ring Gradient for Countdown Widget.
+            ringGradient: null,
 
-        // Filling Color for Countdown Widget.
-        fillColor: Colors.purpleAccent[100]!,
+            // Filling Color for Countdown Widget.
+            fillColor: Colors.purpleAccent[100]!,
 
-        // Filling Gradient for Countdown Widget.
-        fillGradient: null,
+            // Filling Gradient for Countdown Widget.
+            fillGradient: null,
 
-        // Background Color for Countdown Widget.
-        backgroundColor: Colors.purple[500],
+            // Background Color for Countdown Widget.
+            backgroundColor: Colors.purple[500],
 
-        // Background Gradient for Countdown Widget.
-        backgroundGradient: null,
+            // Background Gradient for Countdown Widget.
+            backgroundGradient: null,
 
-        // Border Thickness of the Countdown Ring.
-        strokeWidth: 20.0,
+            // Border Thickness of the Countdown Ring.
+            strokeWidth: 20.0,
 
-        // Begin and end contours with a flat edge and no extension.
-        strokeCap: StrokeCap.round,
+            // Begin and end contours with a flat edge and no extension.
+            strokeCap: StrokeCap.round,
 
-        // Text Style for Countdown Text.
-        textStyle: TextStyle(
-            fontSize: 33.0, color: Colors.white, fontWeight: FontWeight.bold),
+            // Text Style for Countdown Text.
+            textStyle: TextStyle(
+                fontSize: 33.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold),
 
-        // Format for the Countdown Text.
-        textFormat: CountdownTextFormat.S,
+            // Format for the Countdown Text.
+            textFormat: CountdownTextFormat.S,
 
-        // Handles Countdown Timer (true for Reverse Countdown (max to 0), false for Forward Countdown (0 to max)).
-        isReverse: false,
+            // Handles Countdown Timer (true for Reverse Countdown (max to 0), false for Forward Countdown (0 to max)).
+            isReverse: true,
 
-        // Handles Animation Direction (true for Reverse Animation, false for Forward Animation).
-        isReverseAnimation: false,
+            // Handles Animation Direction (true for Reverse Animation, false for Forward Animation).
+            isReverseAnimation: true,
 
-        // Handles visibility of the Countdown Text.
-        isTimerTextShown: true,
+            // Handles visibility of the Countdown Text.
+            isTimerTextShown: true,
 
-        // Handles the timer start.
-        autoStart: false,
+            // Handles the timer start.
+            autoStart: false,
 
-        // This Callback will execute when the Countdown Starts.
-        onStart: () {
-          // Here, do whatever you want
-          print('Countdown Started');
-        },
+            // This Callback will execute when the Countdown Starts.
+            onStart: () {
+              setState(() {
+                _title = "Pause";
+              });
+            },
 
-        // This Callback will execute when the Countdown Ends.
-        onComplete: () {
-          // Here, do whatever you want
-          print('Countdown Ended');
-        },
-      )),
+            // This Callback will execute when the Countdown Ends.
+            onComplete: () {
+              // Here, do whatever you want
+              print('Countdown Ended');
+            },
+          ),
+        ),
+      ),
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
@@ -141,11 +119,21 @@ class _MyHomePageState extends State<MyHomePage> {
           SizedBox(
             width: 10,
           ),
-          _button(title: "Pause", onPressed: () => _controller.pause()),
-          SizedBox(
-            width: 10,
-          ),
-          _button(title: "Resume", onPressed: () => _controller.resume()),
+          _button(
+              title: _title,
+              onPressed: () {
+                if (_title == "Pause") {
+                  _controller.pause();
+                  setState(() {
+                    _title = "Resume";
+                  });
+                } else {
+                  _controller.resume();
+                  setState(() {
+                    _title = "Pause";
+                  });
+                }
+              }),
           SizedBox(
             width: 10,
           ),
@@ -159,13 +147,12 @@ class _MyHomePageState extends State<MyHomePage> {
 
   _button({required String title, VoidCallback? onPressed}) {
     return Expanded(
-        child: RaisedButton(
+        child: ElevatedButton(
       child: Text(
         title,
         style: TextStyle(color: Colors.white),
       ),
       onPressed: onPressed,
-      color: Colors.purple,
     ));
   }
 }
