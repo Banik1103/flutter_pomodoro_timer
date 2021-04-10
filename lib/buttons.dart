97eb54d17eda9e175216f2fbbe9816ad5623080a
Banik1103 <jeremi.kilimnik@gmail.com>
2021-04-10 19:22:28 +0200

    added new file with buttons

---
+++ lib/buttons.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ControlButton extends StatelessWidget {
+  final String title;
+  final color;
+  final String buttonText;
+  final buttonTapped;
+
+  ControlButton(
+      {required this.title,
+      this.color,
+      required this.buttonText,
+      this.buttonTapped});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ElevatedButton(
+        onPressed: buttonTapped,
+        child: Text(
+          title,
+          style: TextStyle(color: color),
+        ),
+      ),
+    );
+  }
+}
